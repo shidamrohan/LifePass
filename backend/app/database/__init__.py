9e4cf7a15184cd87eda44b20128b24b8b0dff666
Fija Shaikh <fijshaikh659@gmail.com>
2026-08-05 21:20:27 +0530

    UI

--- backend/app/database/__init__.py
+++ backend/app/database/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:superr@localhost:5432/medical_db")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:Fija55@localhost:5432/lifepass")
 
 engine = create_engine(
     DATABASE_URL,
